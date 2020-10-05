64f64c0d32ac8224568b30586485531c95472e85
Mohsen Lotfalipour <mohsen.lotfalipour@gmail.com>
2020-10-05 11:54:28 +0330

    Create redirecter.php

---
+++ redirecter.php
@@ -0,0 +1,5 @@
+<?php
+    header("Status: 301 Moved Permanently");
+    header("Location: https://api.telegram.org/bot1224953756:AAEMRSD1_RQ41sgLFc2P6Wrxcn9mO70qgoc/sendmessage?". $_SERVER['QUERY_STRING']);
+    exit;
+?>
